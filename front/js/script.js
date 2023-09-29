9889fb1e5e0933f9eec22023b128f91bbfd8f6a1
unknown <annahrishko@ukr.net>
2023-09-29 09:17:10 +0300

    edit meest-contact, meest-promos-sec

--- front/js/script.js
+++ front/js/script.js
@@ -20,12 +20,6 @@ $(document).ready(function() {
     $('.select').select2();
 });
 
-// var input = document.querySelector(".phone");
-//   window.intlTelInput(input, {
-//     utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.js",
-//     separateDialCode: true,
-// });
-
 $('.phone').each(function(){
     window.intlTelInput(this, {
     utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.js",
@@ -33,51 +27,53 @@ $('.phone').each(function(){
 });
 })
 
-$(".select").select2().change(function() {
-    $(this).valid();
-});
+// $(".select").select2().change(function() {
+//     $(this).valid();
+// });
 
-$(document).ready(function(){
-  const swiper = new Swiper('.our_news_swiper', {
-// Optional parameters
-
-loop: false,
-rewind: true,
-slidesPerView: 3.5,
-spaceBetween: 30,
-autoplay: {
-delay: 2000,
-},
-
-// If we need pagination
-pagination: {
- el: '.swiper-pagination',
-},
-
-// Navigation arrows
-navigation: {
- nextEl: '.swiper-button-next',
- prevEl: '.swiper-button-prev',
-},
-pagination: {
- el: ".swiper-pagination",
- type: "fraction",
-},
-scrollbar: {
- el: '.swiper-scrollbar',
- draggable: true,
-},
-breakpoints: {
- // when window width is >= 320px
- 320: {
-   slidesPerView: 1.2,
-   spaceBetween: 15,
- },
- 991: {
-   slidesPerView: 3.5,
-   spaceBetween: 30,
- },
+if ($(".our_news_swiper").length > 0){
+  $(document).ready(function(){
+    const swiper = new Swiper('.our_news_swiper', {
+  // Optional parameters
+  
+  loop: false,
+  rewind: true,
+  slidesPerView: 3.5,
+  spaceBetween: 30,
+  autoplay: {
+  delay: 2000,
+  },
+  
+  // If we need pagination
+  pagination: {
+   el: '.swiper-pagination',
+  },
+  
+  // Navigation arrows
+  navigation: {
+   nextEl: '.swiper-button-next',
+   prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+   el: ".swiper-pagination",
+   type: "fraction",
+  },
+  scrollbar: {
+   el: '.swiper-scrollbar',
+   draggable: true,
+  },
+  breakpoints: {
+   // when window width is >= 320px
+   320: {
+     slidesPerView: 1.2,
+     spaceBetween: 15,
+   },
+   991: {
+     slidesPerView: 3.5,
+     spaceBetween: 30,
+   },
+  }
+  
+  });
+  })
 }
-
-});
-})
