cbba45d18802db18bb1dce1320fea0fdbe1d14f6
unknown <annahrishko@ukr.net>
2023-11-21 15:22:46 +0200

    edit become-partner(bcg-img), add validation form

---
+++ front/js/script-become-partner.js
@@ -0,0 +1,68 @@
+
+// $(document).ready(function() {
+//     $('.select').select2();
+// });
+// $(".select").select2().change(function() {
+//     $(this).valid();
+// });
+$(function() {
+    $.validator.addMethod("emailRegex", function(value, element) {
+        return this.optional(element) || /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/i.test(value);
+    }, "");
+  });
+$(function() {
+    $(".form-become-partner").validate({
+        rules: {
+            "represent": {
+                required: true,
+            },
+            "looking-for": {
+                required: true,
+            },
+            "location": {
+                required: true,
+            },
+            "name": {
+                required: true,
+                minlength: 3,
+            },
+            "phone": {
+                required: true,
+                minlength: 11,
+            },
+            "email": {
+                required: true,
+                emailRegex: true,
+            },
+            "pr-policy-partner": {
+              required: true,
+            }
+        },
+        messages: {
+            "represent": {
+                required: "Required",
+            },
+            "looking-for": {
+                required: "Required",
+            },
+            "location": {
+                required: "Required",
+            },
+            "name": {
+                required: "Required field",
+                minlength: "Enter your full name",
+            },
+            "phone": {
+                required: "Required field",
+                minlength: "Enter your full phone number",
+            },
+            "email": {
+                required: "Required",
+                emailRegex: "Enter full email",
+            },
+            "pr-policy-partner": {
+              required: "Required",
+            }
+        },
+    });
+  });
